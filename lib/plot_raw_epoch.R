5683c2ee82911d9abb6910f6040ded53045d51c7
irishlouis <louissmith@gmail.com>
2017-05-25 21:07:10 +0100

    Break out code to plot vector magnitude for single epoch to a function

---
+++ lib/plot_raw_epoch.R
@@ -0,0 +1,24 @@
+#' Title
+#'
+#' @param dt 
+#' @param epoch_id 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_raw_epoch <- function(dt, epoch_id){
+  # plot raw data for single epoch
+  p1 <- dt[epoch_id == epoch_id] %>%
+    ggplot(aes(datetime, vec.mag, group = device_id)) + 
+    geom_line() +
+    facet_wrap(~device_id, ncol = 1) + 
+    labs(title = paste("Raw data for a epoch", epoch_id),
+         y = "Accelerometer Vector Magnitude",
+         x = "Time (s)") + 
+    theme_bw() +
+    theme(legend.position = "bottom",
+          legend.key = element_rect(colour = "white"),
+          panel.grid.minor = element_blank())
+  return(p1)
+}
