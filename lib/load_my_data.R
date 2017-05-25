c49d451016de884716d90461158c2a174e028135
irishlouis <louissmith@gmail.com>
2017-05-25 20:43:09 +0100

    changes to load_my_data.R, adding epoch_length as argument and removing joining epoch data

--- lib/load_my_data.R
+++ lib/load_my_data.R
@@ -1,35 +1,32 @@
-#' load.data
+
+#' Title
 #'
-#' @param datafolder, name of subfolder within /data to read from 
+#' @param datafolder 
+#' @param epoch_length 
 #'
-#' @return data
+#' @return
 #' @export
 #'
 #' @examples
-load.data <- function(datafolder = NULL){
+load.data <- function(datafolder = NULL, epoch_length){
   ifelse (!is.null(datafolder),
-    csv.files <- paste0("data/", datafolder,"/", list.files(paste0("data/", datafolder))),
-    csv.files <- paste0("data/", list.files(paste0("data"))))
+          csv.files <- paste0("data/", datafolder,"/", list.files(paste0("data/", datafolder))),
+          csv.files <- paste0("data/", list.files(paste0("data"))))
   
-  # get the summary files
-  epoch <- lapply(csv.files[str_detect(csv.files, "5sec") * str_detect(csv.files, "csv")], fread, header = TRUE, stringsAsFactors = FALSE) %>%
-    rbindlist
-  epoch <- epoch[,':='(
-    timestamp = ymd_hms(timestamp))][,
-                                     .(timestamp, serialnumber, steps),][,
-                                                                         steps_bin :=bin_steps(steps, 0, 20, 3),]
-  setkey(epoch, serialnumber, timestamp)
+  # get the csv files
+  csv.files <- csv.files[grep(".csv", csv.files)]
   
   # empty list to hold raw data
   data <- list()
   # allow for milliseconds
   options(digits.secs=3)
   
+  # for each RAW csv file
   for (i in csv.files[str_detect(csv.files, "RAW.csv")]){
     dt <- fread(i, header = T, stringsAsFactors = F)
     
-    device_id <- strsplit(strsplit(i, " ")[[1]][1], "/")[[1]][ifelse(is.null(datafolder), 
-                                                                     0, 
+    device_id <- strsplit(strsplit(i, " ")[[1]][1], "/")[[1]][ifelse(is.null(datafolder),
+                                                                     0,
                                                                      length(strsplit(datafolder, "/")))+2]
     dt[,':='(
       device_id = device_id,
@@ -41,7 +38,7 @@ load.data <- function(datafolder = NULL){
       `Accelerometer Z` = NULL
     ),][,':='(
       time_minute = floor_date(datetime, "minute"),
-      second = 5 * floor(second(datetime)/5)
+      second = epoch_length * floor(second(datetime)/epoch_length)
     ),][,':='(
       epoch_id = time_minute + seconds(second),
       time_minute = NULL,
@@ -54,9 +51,5 @@ load.data <- function(datafolder = NULL){
   # collapse to single file
   data <- rbindlist(data)
   setkey(data, device_id, epoch_id)
-  
-  # join epoch steps to raw
-  data <- data[epoch] 
-  
   return(data)
 }
