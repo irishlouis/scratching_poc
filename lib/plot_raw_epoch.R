12fd4ec47350925988dba3f68f7704c3f4002af6
irishlouis <louissmith@gmail.com>
2017-05-26 22:10:40 +0100

    brake out function to plot raw epoch

--- lib/plot_raw_epoch.R
+++ lib/plot_raw_epoch.R
@@ -7,13 +7,13 @@
 #' @export
 #'
 #' @examples
-plot_raw_epoch <- function(dt, epoch_id){
+plot_raw_epoch <- function(dt, epoch){
   # plot raw data for single epoch
-  p1 <- dt[epoch_id == epoch_id] %>%
+  p1 <- dt[epoch_id == epoch] %>%
     ggplot(aes(datetime, vec.mag, group = device_id)) + 
     geom_line() +
     facet_wrap(~device_id, ncol = 1) + 
-    labs(title = paste("Raw data for a epoch", epoch_id),
+    labs(title = paste("Raw data for a epoch", epoch),
          y = "Accelerometer Vector Magnitude",
          x = "Time (s)") + 
     theme_bw() +
