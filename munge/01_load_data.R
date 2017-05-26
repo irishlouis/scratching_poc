fc071a85a1addf5e48819c714f429da8227054d6
irishlouis <louissmith@gmail.com>
2017-05-26 22:11:13 +0100

    wip on load_data.r

--- munge/01_load_data.R
+++ munge/01_load_data.R
@@ -1,16 +1,8 @@
 # get csv files from /data
 
-# load training / testing dataset 
-## data from louis and triona and 1 dog
-train.data <- load.data(datafolder = "louis1")
-cache("train.data")
-
-# load eval dataset 
-## data from louis and triona and 1 dog
-eval1.data <- load.data(datafolder = "louis2")
-cache("eval1.data")
-
-# load final evaluation dataset for unseen subjects
-## data from marie and 2 dogs
-eval2.data <- load.data(datafolder = "marie")
-cache("eval2.data")
+## load data for POC
+### raw data
+poc.raw.data <- load.data(datafolder = "POC", epoch_length = 10 )
+cache("poc.raw.data")
+### subject diary data
+poc.subj.diary <- fread("")
