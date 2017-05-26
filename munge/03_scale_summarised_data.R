20807efd373a5b26bba3db7714e8fa688d05adbe
irishlouis <louissmith@gmail.com>
2017-05-26 22:16:19 +0100

    add 03 scale summarised data.r

---
+++ munge/03_scale_summarised_data.R
@@ -0,0 +1,10 @@
+# prepare data for modelling
+scale.cols <- c("avg.vec","sd.vec","peaks.per.sec",
+                "avg.period","sd.period","avg.amp",
+                "sd.amp")
+
+poc.summary[, (scale.cols) := lapply(.SD, scale), , 
+            .SDcols = scale.cols]
+
+head(poc.summary)
+cache("poc.summary")
