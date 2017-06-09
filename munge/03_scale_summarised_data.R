58edd9f67c177f702888a07d2158d2579c65ee31
irishlouis <louissmith@gmail.com>
2017-06-09 08:35:56 +0100

    merge 03_scale_summarised

--- munge/03_scale_summarised_data.R
+++ munge/03_scale_summarised_data.R
@@ -1,14 +1,24 @@
-# prepare data for modelling
-scale.cols <- c("avg.vec","sd.vec","peaks.per.sec",
-                "avg.period","sd.period","avg.amp",
-                "sd.amp")
+# select columns to be scaled
+scale.cols <- names(poc.summary)[sapply(poc.summary, is.numeric)]
 
-poc.summary[, (scale.cols) := lapply(.SD, scale), , 
-            .SDcols = scale.cols]
+# z scale each column for each of the summary data sets
+zscale.poc.summary <- copy(poc.summary)[,(scale.cols) := lapply(.SD, scale), , .SDcols = scale.cols]
+cache("zscale.poc.summary")
 
+<<<<<<< HEAD
 
 test2.summary[, (scale.cols) := lapply(.SD, scale), , .SDcols = scale.cols]
 
 head(poc.summary)
 cache("poc.summary")
 rm(scale.cols)
+=======
+zscale.test1.summary <- copy(test1.summary)[,(scale.cols) := lapply(.SD, scale), , .SDcols = scale.cols]
+cache("zscale.test1.summary")
+
+zscale.test2.summary <- copy(test2.summary)[,(scale.cols) := lapply(.SD, scale), , .SDcols = scale.cols]
+cache("zscale.test2.summary")
+
+# housekeeping
+rm(scale.cols)
+>>>>>>> 98fe6ad72b7e365e930a9497e35a86b372d55e4a
