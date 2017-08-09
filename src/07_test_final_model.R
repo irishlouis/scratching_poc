fbcfb5d42ba15207b96c463b027dc7870330d138
irishlouis <louissmith@gmail.com>
2017-08-09 10:20:06 +0100

    add scratching events to unseen device

--- src/07_test_final_model.R
+++ src/07_test_final_model.R
@@ -19,3 +19,19 @@ predicted_scratching_events <- dplyr::bind_rows(
   arrange(device_id, epoch_id)
 
 cache("predicted_scratching_events")
+
+predicted_scratching_events$epoch_id[predicted_scratching_events$device_id == "TAS1E31150003"]
+
+scratching.times <- sapply(c("2017-06-02 03:26:00 UTC", "2017-06-02 03:26:10 UTC", "2017-06-02 03:26:20 UTC", "2017-06-02 03:26:30 UTC", 
+                             "2017-06-02 03:28:00 UTC", "2017-06-02 03:28:10 UTC", "2017-06-02 03:28:20 UTC", "2017-06-02 03:28:30 UTC", 
+                             "2017-06-02 03:30:00 UTC", "2017-06-02 03:30:10 UTC", "2017-06-02 03:30:20 UTC", "2017-06-02 03:30:30 UTC", 
+                             "2017-06-02 03:56:00 UTC", "2017-06-02 03:56:10 UTC", "2017-06-02 03:56:20 UTC", "2017-06-02 03:56:30 UTC", 
+                             "2017-06-02 04:07:00 UTC", "2017-06-02 04:07:10 UTC", "2017-06-02 04:07:20 UTC", "2017-06-02 04:07:30 UTC", 
+                             "2017-06-02 04:27:00 UTC", "2017-06-02 04:27:10 UTC", "2017-06-02 04:27:20 UTC", "2017-06-02 04:27:30 UTC", 
+                             "2017-06-02 06:10:00 UTC", "2017-06-02 06:10:10 UTC", "2017-06-02 06:10:20 UTC", "2017-06-02 06:10:30 UTC"), ymd_hms)
+
+test2.summary[epoch_id %in% scratching.times]
+
+confusionMatrix(test2.summary$prediction, 
+factor(sapply(test2.summary$epoch_id, function(x) ifelse( x %in% scratching.times,
+"scratch", "not_scratch"))))
